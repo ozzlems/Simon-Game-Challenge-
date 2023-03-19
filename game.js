084511b603d71ca0ea23fc461fa000f13b0d6101
ozzlems <ozlemsencoruh@gmail.com>
2023-03-20 01:59:13 +0300

    added click function

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+
 var gamePattern = [];
 var buttonColours = ["red" , "blue" , "green" , "yellow"];
 var randomChosenColour;
@@ -6,3 +7,14 @@ function nextSequence(){
 }
 randomChosenColour= buttonColours[randomNumber];
 gamePattern.push(randomChosenColour);
+
+$("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+$("h2").css("color", "red");
+
+var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+audio.play();
+
+$("button").click(function(){
+var userChosenColour = $("#" + randomChosenColour);
+
+});
