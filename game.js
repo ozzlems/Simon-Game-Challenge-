98b6073aa6af9c6932f1af27497b9cb71b59f6cd
ozzlems <ozlemsencoruh@gmail.com>
2023-03-20 11:34:31 +0300

    added timed animation

--- game.js
+++ game.js
@@ -2,20 +2,33 @@ var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ["red" , "blue" , "green" , "yellow"];
 var randomChosenColour;
+
+
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4);
-}
-randomChosenColour= buttonColours[randomNumber];
+    randomChosenColour= buttonColours[randomNumber];
 gamePattern.push(randomChosenColour);
 
 $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-$("h2").css("color", "red");
+playSound(randomChosenColour);
+}
 
-var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-audio.play();
 
 $(".btn").click(function(){
 var userChosenColour = $(this).attr("id");
 
 userClickedPattern.push(userChosenColour);
+playSound(userChosenColour);
 });
+
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+audio.play();
+}
+
+function animatePress(currentColour){
+   $("#" + currentColour).addClass("pressed");
+   setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed"); } , 100);
+   }
+    
