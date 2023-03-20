72b1f2c1cdaa7c346b10938c099daefb76228428
ozzlems <ozlemsencoruh@gmail.com>
2023-03-20 13:31:23 +0300

    added level upgrade

--- game.js
+++ game.js
@@ -2,9 +2,13 @@ var userClickedPattern = [];
 var gamePattern = [];
 var buttonColours = ["red" , "blue" , "green" , "yellow"];
 var randomChosenColour;
+var started = false;
+var level = 0;
 
 
 function nextSequence(){
+    level++;
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random() * 4);
     randomChosenColour= buttonColours[randomNumber];
 gamePattern.push(randomChosenColour);
@@ -19,6 +23,7 @@ var userChosenColour = $(this).attr("id");
 
 userClickedPattern.push(userChosenColour);
 playSound(userChosenColour);
+animatePress(userChosenColour);
 });
 
 function playSound(name){
@@ -29,6 +34,14 @@ audio.play();
 function animatePress(currentColour){
    $("#" + currentColour).addClass("pressed");
    setTimeout(function(){
-    $("#" + currentColour).removeClass("pressed"); } , 100);
+    $("#" + currentColour).removeClass("pressed"); } , 150);
    }
+
+   $(document).keypress(function(){
+    if(!started){
+        $("#level-title").text("Level " + level );
+        nextSequence();
+        started = true;
+    }}
+   );
     
